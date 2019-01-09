f246250dc2b1a6e9620559577803d27c843c52d1
Grace Hartzell <gracehartzell@utexas.edu>
2019-01-09 13:32:28 -0600

    Insert method test complete

--- test/Tries.test.js
+++ test/Tries.test.js
@@ -1,14 +1,72 @@
 const expect = require("chai").expect; // eslint-disable-line
-const Trie = require("../trie.js");
+const { Node, Trie } = require("../trie.js");
 
-describe("Basic", () => {
-  it("should add correctly", () => {
-    expect(1 + 1).to.eql(2);
+const testTrie = new Trie();
+
+describe("Node for Trie", () => {
+  it("should create a new node", () => {
+    const node = new Node();
+    expect(node).to.eql({ key: "", parent: null, children: [], end: false });
   });
 });
-
 describe("Trie", () => {
   it("can be initialized", () => {
-    const t = new Trie();
+    expect(testTrie).to.eql({
+      root: { key: "", parent: null, children: [], end: false }
+    });
+  });
+  it("should insert word to Trie", () => {
+    let firstWord = testTrie.insert("a");
+    let secondWord = testTrie.insert("and");
+    let thirdWord = testTrie.insert("be");
+
+    expect(firstWord).to.eq({
+      root: {
+        key: "",
+        parent: null,
+        children: [{ key: "a", parent: null, children: [], end: true }],
+        end: false
+      }
+    });
+    expect(testTrie.root.children).to.have.length(1);
+
+    expect(secondWord).to.eq({
+      root: {
+        key: "",
+        parent: null,
+        children: [
+          {
+            key: "a",
+            parent: null,
+            children: [
+              {
+                key: "n",
+                parent: null,
+                children: [{ key: "d", parent: null, children: [], end: true }],
+                end: false
+              }
+            ],
+            end: true
+          }
+        ],
+        end: false
+      }
+    });
+    expect(thirdWord).to.eq({
+      root: {
+        key: "",
+        parent: null,
+        children: [
+          {
+            key: "b",
+            parent: null,
+            children: [{ key: "e", parent: null, children: [], end: true }],
+            end: false
+          }
+        ],
+        end: false
+      }
+    });
+    expect(testTrie.root.children).to.have.length(2);
   });
 });
--- trie.js
+++ trie.js
@@ -1,10 +1,16 @@
-class Trie {
+class Node {
   constructor(key) {
-    this.key = "",
-    this.parent = null,
-    this.children = [],
-    this.end = false;
+    (this.key = ""),
+      (this.parent = null),
+      (this.children = []),
+      (this.end = false);
   }
 }
 
-module.exports = Trie;
+class Trie {
+  constructor() {
+    this.root = new Node(null);
+  }
+}
+
+module.exports = { Node, Trie };

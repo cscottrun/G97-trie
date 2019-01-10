a8ad03ae159158a871e74ac4b2dda19291c8d142
Carrie Scott <carriescott@carries-mbp.home>
2019-01-09 21:07:54 -0600

    insert working

--- trie.js
+++ trie.js
@@ -1,24 +1,33 @@
 class Node {
   constructor(key) {
-    (this.key = ""),
-      (this.parent = null),
-      (this.children = []),
-      (this.end = false);
+    this.key= key,
+    this.children= {},
+    this.end= false
   }
-}
+};
 
 class Trie {
   constructor() {
     this.root = new Node(null);
   }
-  insert(word) {
-    let node = this.root;
-    for (let i = 0 ; i < word.length; i++) {
-      if (!node.children[word[i]]) {
-        node.children[word[i]] = Node
-      }
-    }
-  }
+
+  insert (word) {
+	  let  node = this.root;
+	  for(var i = 0; i < word.length; i++) {
+	    if (!node.children[word[i]]) {
+	      node.children[word[i]] = new Node(word[i]);
+	    }
+	    node = node.children[word[i]];
+	    if (i == word.length-1) {
+	      node.end = true;
+	    }
+	  }
+  };
 }
 
+let myTrie = new Trie;
+myTrie.insert('a');
+myTrie.insert('and');
+
+console.log(myTrie.root.children.a)
 module.exports = { Node, Trie };
